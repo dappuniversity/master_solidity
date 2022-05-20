ab7f907194a4ee5b91398fa2c5c733de21434c61
Justin <48664609+GlitchicaL@users.noreply.github.com>
2022-05-20 14:34:52 -0400

    Added comments

--- examples/6_HotelRoom.sol
+++ examples/6_HotelRoom.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Showcase payment transfers, enums, modifiers & events
+
 contract HotelRoom {
     enum Statuses {
         Vacant,
--- examples/7_Secret.sol
+++ examples/7_Secret.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Showcase contract inheritance
+
 contract Ownable {
     address owner;
 
